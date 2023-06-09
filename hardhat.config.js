618d688e8127fa7e8689d1628579da3940d9181d
kylin <kylin@gmail.com>
2023-06-09 15:51:49 +0800

    :sparkles: feat(1.0): 完成合约，网络等部分
    Signed-off-by: kylin <kylin@gmail.com>

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+    const accounts = await ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.18",
+};
